0bed9d2f5e5166cbda9ce95a753e7a66fb99b496
Crowdbotics <team@crowdbotics.com>
2023-09-26 21:28:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "besthro-43584",
+  url: "https://besthro-43584.botics.co",
+  api: "https://besthro-43584.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
